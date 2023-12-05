2ca77868238e7f7b685720677eca5237e0c47172
Yu Qiao <Geselle_joy@aliyun.com>
2023-12-05 23:13:42 +0800

    Update compressor_pdf.py

--- usage/compressor_pdf.py
+++ usage/compressor_pdf.py
@@ -1,6 +1,5 @@
 # -*- coding:UTF-8 -*-
 import os, sys, shutil
-import subprocess
 from jmcomic.cl import get_env
   
 def get_all_pdfs(directory):  
@@ -15,40 +14,10 @@ def get_all_pdfs(directory):
 
     return pdfs
 
-def compress(source, output):
-    compressPdfDocument = ap.Document(source)  # 需要压缩的pdf文件路径
-    pdfoptimizeOptions = ap.optimization.OptimizationOptions()
-    pdfoptimizeOptions.image_compression_options.compress_images = True
-    pdfoptimizeOptions.image_compression_options.image_quality = 10
-    compressPdfDocument.optimize_resources(pdfoptimizeOptions)
-    compressPdfDocument.save(output)  # 需要压缩后保存的文件路径
-
-def get_ghostscript_path():
-    gs_names = ['gs', 'gswin32', 'gswin64']
-    for name in gs_names:
-        if shutil.which(name):
-            return shutil.which(name)
-    raise FileNotFoundError(f'No GhostScript executable was found on path ({"/".join(gs_names)})')
-
-
 def main():
     root = get_env('JM_DOWNLOAD_DIR', None)
     pdfs = get_all_pdfs(root)
-    # gs = get_ghostscript_path()
-    # quality = {
-    #     0: '/default',
-    #     1: '/prepress',
-    #     2: '/printer',
-    #     3: '/ebook',
-    #     4: '/screen'
-    # }
     for pdf in pdfs:
-        # file_name = os.path.basename(pdf)[:-4]
-        # directory = os.path.dirname(pdf)
-        # compress_pdf = os.path.join(directory, file_name + '-compress.pdf')
-        # print(f"源文件: {pdf}, 压缩文件: {compress_pdf}")
-        # compress(pdf, compress_pdf)
-
         val = os.system(f'pdf-compressor --compression-level low "{pdf}"')
         print(val)
         os.remove(pdf)
