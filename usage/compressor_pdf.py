db6e71ab4e6ea08b48f3d2bdfb34d94c52c5eb70
Yu Qiao <Geselle_joy@aliyun.com>
2023-12-05 15:22:26 +0800

    add compressor_pdf.py

---
+++ usage/compressor_pdf.py
@@ -0,0 +1,38 @@
+import aspose.pdf as ap
+import os, sys
+  
+def get_all_pdfs(directory):  
+    pdfs = []  # 用来存放所有PDF文件的列表  
+  
+    # 使用os.walk()函数遍历目录及其所有子目录  
+    for dirpath, dirnames, filenames in os.walk(directory):  
+        # 使用glob.glob()函数找到所有PDF文件  
+        for filename in filenames:  
+            if filename.endswith('.pdf'):  # 如果文件以'.pdf'结尾  
+                pdfs.append(os.path.join(dirpath, filename))  # 添加文件路径到列表中  
+
+    return pdfs
+
+def compress(source, output):
+	compressPdfDocument = ap.Document(source)  # 需要压缩的pdf文件路径
+	pdfoptimizeOptions = ap.optimization.OptimizationOptions()
+	pdfoptimizeOptions.image_compression_options.compress_images = True
+	pdfoptimizeOptions.image_compression_options.image_quality = 10
+	compressPdfDocument.optimize_resources(pdfoptimizeOptions)
+	compressPdfDocument.save(output)  # 需要压缩后保存的文件路径
+
+def main():
+	root = get_env('JM_DOWNLOAD_DIR', None)
+	pdfs = get_all_pdfs(root)
+	for pdf in pdfs:
+		file_name = os.path.basename(pdf)[:-4]
+		directory = os.path.dirname(pdf)
+		compress_pdf = os.path.join(directory, file_name + '_compress.pdf')
+		compress(pdf, compress_pdf)
+		os.remove(pdf)
+
+if __name__ == '__main__':
+    main()
+
+
+
